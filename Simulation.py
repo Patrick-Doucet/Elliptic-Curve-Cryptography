b45fd6a25b4c455d16572a67d095aea84c98cc7b
Patrick Doucet <patrick.doucet6@gmail.com>
2018-04-06 18:12:16 -0300

    Added scripts for Crypto use

---
+++ Simulation.py
@@ -0,0 +1,17 @@
+#Simulation of Elliptic Curve Cryptography
+
+from EllipticCurve import *
+
+
+def diffieHellmanExchange():
+    return
+
+
+def threePassProtocol():
+    return
+
+def main():
+    print("hello world!")
+
+if __name__ == "__main__":
+    main()
