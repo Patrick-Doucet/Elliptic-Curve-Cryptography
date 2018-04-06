72bc138c5f16b5bdfc6ea3af242eb4fffab36367
Patrick Doucet <patrick.doucet6@gmail.com>
2018-04-06 19:49:46 -0300

    Added init functions for classes, started class functions

--- EllipticCurve.py
+++ EllipticCurve.py
@@ -1,2 +1,38 @@
 #Elliptic Curve Class
+class EllipticCurve:
 
+    def __init__(self, p, a, b, G, n, h):
+        self.p = p
+        self.a = a
+        self.b = b
+        self.G = G
+        self.n = n
+        self.h = h
+    
+
+    
+    def Secant(self, point1, point2):
+        return
+
+    def Tangent(self, point):
+        return
+
+class Point(EllipticCurve):
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        #Don't delete this
+
+        #if self != other and self != 0 and other != 0:
+            #Secant(self, other)
+        #if self == other:
+            #Tangent(self)
+        #if self == 0 and other != 0:
+            #Tangent(other)
+        #if self != 0 and other == 0:
+            #Tangent(self)
+        
+        return
--- Simulation.py
+++ Simulation.py
@@ -2,8 +2,13 @@
 
 from EllipticCurve import *
 
+dashString = "-"*40
 
 def diffieHellmanExchange():
+    #Do the exchange, store in variables for the prints
+
+    #print Ka, then Kab, then Kba, then Kb
+    
     return
 
 
@@ -11,7 +16,15 @@ def threePassProtocol():
     return
 
 def main():
-    print("hello world!")
+
+    print("Diffie Hellman Key Exchange\n" + dashString)
+    diffieHellmanExchange()
+    #For simulation purpose, input() acts as a system pause
+    input()
+
+    print("The curve created in this simulation is secp128r1")
+    
+
 
 if __name__ == "__main__":
     main()
